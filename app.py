308973101f979b569af7d8db7b2bb9b922f7af34
Piotr <p.szymanski135@gmail.com>
2020-06-12 20:16:25 +0200

    coors

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ from flask import Flask
 from flask_restful import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS, cross_origin
 from database.db import initialize_db
 from resources.routes import initialize_routes
 from resources.errors import errors
 
 app = Flask(__name__)
-
+cors = CORS(app)
 app.config["MONGODB_SETTINGS"] = {
     "host": "mongodb://localhost/meetings"
 }
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.config["JWT_SECRET_KEY"] = "ultrahiperjwtsecretkeyformongodbproject2137xD"
 
 api = Api(app, errors=errors)
